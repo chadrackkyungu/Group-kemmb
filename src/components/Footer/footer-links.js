8fdd5fbf70141dc5e7407ec7c1421efc0fd59558
JohnDevJs <JohnDevJs@users.noreply.github.com>
2024-03-05 23:26:04 +0200

    added copyright

--- src/components/Footer/footer-links.js
+++ src/components/Footer/footer-links.js
@@ -1,16 +1,24 @@
-import React, { Component } from 'react';
-import "./footer.css"
+import React, { Component } from "react";
+import "./footer.css";
 
 class FooterLinks extends Component {
-    render() {
-        return (
-            <React.Fragment>
-                <div className="footer-alt bg-dark">
-                    <p className="copy-rights"> 2022 © palmtr3</p>
-                </div>
-            </React.Fragment>
-        );
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <div className="footer-alt bg-dark">
+          <p className="copy-rights">
+            © Copyright 2024 build with love ❤️ by{" "}
+            <a
+              href="https://www.websitebuilders.dev"
+              style={{ color: "black" }}
+            >
+              https://www.websitebuilders.dev
+            </a>
+          </p>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default FooterLinks;
--- src/components/Get in Touch/get-in-touch.js
+++ src/components/Get in Touch/get-in-touch.js
@@ -2,29 +2,31 @@ import React, { useRef } from "react";
 import { Container, Row, Col, FormGroup, Label } from "reactstrap";
 import { AvForm, AvField } from "availity-reactstrap-validation";
 import { Animated } from "react-animated-css";
-import "./Get in touch.css"
-import emailjs from '@emailjs/browser';
+import "./Get in touch.css";
+import emailjs from "@emailjs/browser";
 
 //Import Section Title
 import SectionTitle from "../common/section-title";
 
 function GetInTouch() {
-
   const form = useRef();
 
   const handleValidSubmit = (e, values) => {
     e.preventDefault();
 
     // emailjs.sendForm('gmail', 'template_oixp1mv', form.current, 'YOUR_PUBLIC_KEY')
-    emailjs.sendForm('gmail', 'template_oixp1mv', form.current, 'YOUR_PUBLIC_KEY')
-      .then((result) => {
-        console.log(result.text);
-      }, (error) => {
-        console.log(error.text);
-      });
-    e.target.reset()
-
-  }
+    emailjs
+      .sendForm("gmail", "template_oixp1mv", form.current, "YOUR_PUBLIC_KEY")
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  };
 
   return (
     <React.Fragment>
@@ -36,15 +38,17 @@ function GetInTouch() {
               name="contact-form"
               id="contact-form"
               ref={form}
-              onValidSubmit={(e, v) => { handleValidSubmit(e, v) }}>
-
+              onValidSubmit={(e, v) => {
+                handleValidSubmit(e, v);
+              }}
+            >
               <Row className="Get-in-touch">
                 <Col md="6">
                   <FormGroup>
                     <Label for="name">Phone number</Label>
                     <AvField
                       name="name"
-                      placeholder="Enter your name..."
+                      placeholder="Enter your number"
                       type="text"
                       errorMessage="Name is required"
                       className="form-control"
@@ -90,10 +94,9 @@ function GetInTouch() {
                 </Col>
               </Row>
 
-
               <Row>
                 <div className="btn-center">
-                  <Col sm="12" >
+                  <Col sm="12">
                     <input
                       type="submit"
                       id="submit"
@@ -112,6 +115,5 @@ function GetInTouch() {
     </React.Fragment>
   );
 }
-
 
 export default GetInTouch;
